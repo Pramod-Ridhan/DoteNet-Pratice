8315b951ac82bc94cd17db5f042dcd534d7ef748
Pramod Wayal <pramodwayal90@gmail.com>
2021-03-08 10:12:38 -0800

    Added MVC Template

---
+++ WebGentle.Bookstore/WebGentle.Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebGentle.Bookstore.Controllers
+{
+    public class HomeController:Controller
+    {
+        public string Index()
+        {
+            return "First index Methid";
+        }
+    }
+
+}
--- WebGentle.Bookstore/WebGentle.Bookstore/Startup.cs
+++ WebGentle.Bookstore/WebGentle.Bookstore/Startup.cs
@@ -15,6 +15,7 @@ namespace WebGentle.Bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +25,11 @@ namespace WebGentle.Bookstore
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseRouting();
 
-            app.Run(async (context) =>
+            app.UseEndpoints(endPoint =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                endPoint.MapDefaultControllerRoute();
             });
         }
     }
